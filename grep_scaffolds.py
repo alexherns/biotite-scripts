9dd4bc3b3407552e06b8f4c5be0d826f1061f2d7
alexherns <alexherns@gmail.com>
2015-05-03 22:05:00 -0700

    Added help prompt

--- grep_scaffolds.py
+++ grep_scaffolds.py
@@ -2,12 +2,13 @@
 import sys
 
 if '-h' in sys.argv:
-    print """
+	print """
 grep_scaffolds is a simple wrapper to match lines from a scafs2bins file.
 Usage: grep_scaffolds.py scaffolds.fasta scafs2bins.tsv
 
 Outputs lines from scafs2bins file that match sequence headers from fasta file
 """
+	exit()
 
 scaffolds= set([line.strip().split()[0][1:] for line in open(sys.argv[1]) if line[0]=='>'])
 for line in open(sys.argv[2]):

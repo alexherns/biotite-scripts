18a60fccb838ce3a25570c0848828b6c89b38ade
Alex Hernsdorf <alexherns@gmail.com>
2015-08-02 20:02:09 -0700

    Lots of minor modifications to scripts to port to argparse for library

--- app_each_line.py
+++ app_each_line.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python2.7
-"""Appends each line (first argument) by the string provided (second argument)"""
-import sys
-print "\n".join([line.strip("\n")+sys.argv[2] for line in open(sys.argv[1])])
+import argparse
+parser = argparse.ArgumentParser(description='Appends each line by the string provided', formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=False)
+
+#Required arguments
+required = parser.add_argument_group('REQUIRED')
+required.add_argument('-f', help= 'input file', required=True, type=argparse.FileType('r'))
+required.add_argument('-s', help= 'string to append', required=True, type=str)
+
+#Optional arguments
+optional = parser.add_argument_group('OPTIONAL')
+optional.add_argument('-h', action="help", help="show this help message and exit")
+
+args = parser.parse_args()
+
+print "\n".join([line.strip("\n")+args.s for line in args.f])
--- class_wrapper.py
+++ class_wrapper.py
@@ -1,46 +1,24 @@
 #!/usr/bin/env python2.7
-import sys, colorsys, getopt
+import colorsys, argparse
 import numpy as np
 
-def usage():
-        print '''
-Automatically generates ESOM class file (complete with color-coded bin assignments)
-from ESOM names file and tab-delimited scaffold-bin assignments
+parser = argparse.ArgumentParser(description='generates ESOM class file (with color-coded bins) from ESOM names file and scaffold-bin assignments.', formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=False)
 
-Usage: class_wrapper -n esom.names -s scaffolds2bins.tsv <OPTIONS>
+#Required arguments
+required = parser.add_argument_group('REQUIRED')
+required.add_argument('-n', help= 'esom.names', required=True, type=argparse.FileType('r'))
+required.add_argument('-s', help= 'scaffold-bin assignments', required=True, type=argparse.FileType('r'))
 
-        -h, --help      Print this help dialog
+#Optional arguments
+optional = parser.add_argument_group('OPTIONAL')
+optional.add_argument('-h', action="help", help="show this help message and exit")
+optional.add_argument('--header', help= 'Scaffold-bin file contains header', action='store_true')
 
-        <REQUIRED ARGUMENTS>
-        -n      ESOM Names file
-        -s      List of tab-delimited scaffold-bin assignments
-        
-        <OPTIONS>
-        --header	Scaffold-bin file contains header
+args = parser.parse_args()
 
-'''
-        exit()
-
-opts, args = getopt.getopt(sys.argv[1:], 'hn:s:', ['help', 'header'])
-scaffold_file= ""
-names_file= ""
-header= False
-
-for o, a in opts:
-	if o in ('-h', '--help'):
-		usage()
-	elif o == '-s':
-		scaffold_file = a
-	elif o == '-n':
-		names_file= a
-	elif o == '--header':
-		header= True
-
-if len([o for o, a, in opts])<2:
-        print '''
-This script requires more arguments. Please pass -h or --help for assistance
-'''
-        exit()
+scaffold_file= args.s
+names_file= args.n
+header= args.header
 
 #Function to create an RGB colorspace from a defined number of colors
 def get_colors(num_colors):
@@ -55,7 +33,7 @@ def get_colors(num_colors):
 
 """Dictionary of bins accessed by scaffold
     scaf_dict= {scaffold: bin}"""
-file_handle= open(scaffold_file)
+file_handle= scaffold_file
 if not header:
 	file_handle.readline()
 scaf_list= [line.strip().split() for line in file_handle]
@@ -76,7 +54,7 @@ for org, color in zip(bins, color_space):
     bin_dict[org]["index"]= bins.index(org)+1
 
 #Read in the names file and generate a header for the class file
-file_handle= open(names_file)
+file_handle= names_file
 print file_handle.readline().strip()
 for org in bin_dict:
     print "%\t{0}\t{1}\t{2}".format(str(bin_dict[org]["index"]), org, "\t".join([str(col) for col in bin_dict[org]["rgb"]]))

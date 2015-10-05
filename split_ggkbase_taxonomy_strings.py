dbdcba676947f4afafbbf852f0e4d5057a7b5a1f
alexherns <alexherns@gmail.com>
2015-10-05 11:02:53 -0700

    Extracts the taxonomy string from ggkbase organism_info files into a columnar  format

---
+++ split_ggkbase_taxonomy_strings.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(description='Extracts the taxonomy string from ggkbase organism_info files into a columnar format', formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=False)
+
+#Required arguments
+required = parser.add_argument_group('REQUIRED')
+required.add_argument('-i', help= 'input organism_info sheet', required=True, type=str)
+required.add_argument('-o', help= 'output csv', required=True, type=str)
+
+#Optional arguments
+optional = parser.add_argument_group('OPTIONAL')
+optional.add_argument('-h', action="help", help="show this help message and exit")
+
+args = parser.parse_args()
+
+input_file= args.i
+output_file= args.o
+df= pd.read_csv(input_file, sep='\t', index_col='name', usecols=['name', 'group', 'taxonomy'])
+
+levels= ('domain', 'phylum', 'class', 'order', 'family', 'genus', 'species')
+for level in levels:
+    df[level]= ''
+
+domains= set(['Bacteria', 'Phage', 'Archaea', 'Plasmid-like', 'Eukaryota'])
+for org in df.index:
+    tax_string= df.loc[org].taxonomy
+    tax_list= tax_string.split(", ")[::-1]
+    if tax_list[0] == 'Eukaryote':
+        tax_list[0]= 'Eukaryota'
+    if tax_list[0] not in domains:
+        tax_list.insert(0, 'Bacteria')
+    for i in range(len(tax_list)):
+        df.loc[org][levels[i]]= tax_list[i]
+
+df.to_csv(output_file)

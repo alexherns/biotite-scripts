a59868dc212519a09716e71465f5c5d565fd586c
Alex Hernsdorf <alexherns@gmail.com>
2016-01-02 11:44:50 -0800

    Added functionality for working with USEARCH platform

--- common_objects.py
+++ common_objects.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-"""Script: scaf2bin_dictionary -- Creates a scaffold2bin dictionary object from the respective file"""
+"""Function: scaf2bin_dictionary -- Creates a scaffold2bin dictionary object from the respective file"""
 def scaf2bin_dictionary(file_handle, header=True, sep="\t"):
     if header==True:
         file_handle.readline()
@@ -10,11 +10,11 @@ def scaf2bin_dictionary(file_handle, header=True, sep="\t"):
         scaf2bin[scaffold]= Bin
     return scaf2bin
 
-"""Script: lrn2np -- Reads an esom.lrn file into a numpy array"""
+"""Function: lrn2np -- Reads an esom.lrn file into a numpy array"""
 def lrn2np(file_handle):
 	return np.asarray([[float(i) for i in line.strip().split()[1:]] for line in file_handle if "%" not in line])
 	
-"""Script: writelrn -- Writes an esom.lrn file from a numpy array given an output file handle"""
+"""Function: writelrn -- Writes an esom.lrn file from a numpy array given an output file handle"""
 def writelrn(X, file_handle):
 	num_rows, num_cols= np.shape(X)
 	print num_rows
@@ -23,6 +23,19 @@ def writelrn(X, file_handle):
 	for i, line in enumerate(X.tolist()):
 		file_handle.write('{0}\t{1}\n'.format(str(i+1), '\t'.join([str(j) for j in line])))
 
-"""Script: motif2regex -- Converts a protein motif query to a regex search"""
+"""Function: motif2regex -- Converts a protein motif query to a regex search"""
 def motif2regex(motif):
 	return motif.replace('X', '.').replace('x', '.').replace('(', '{').replace(')', '}')
+
+class UClustSeq():
+    def __init__(self, line):
+        self.cluster= int(line[1])
+        self.query= line[8]
+
+def uclust2dict(file_handle):
+    from collections import defaultdict
+    d= defaultdict(list)
+    for line in file_handle:
+        line= line.strip().split('\t')
+        d[line[1]].append(line[8])
+    return d
